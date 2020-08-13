11894fcf642b49166d9c61ae38c5f70027015009
paramagicdev <konnor5456@gmail.com>
2020-08-13 08:05:36 -0400

    setup for jest / babel

--- jest.config.js
+++ jest.config.js
@@ -24,12 +24,12 @@ module.exports = {
   // coverageDirectory: undefined,
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  // coveragePathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
